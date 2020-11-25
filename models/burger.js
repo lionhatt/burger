7e3b0d77df065188049eaf3c46fb78478244e831
Lionhatt <gavindxy@gmail.com>
2020-11-25 21:51:43 +1100

    create controller

--- models/burger.js
+++ models/burger.js
@@ -3,22 +3,22 @@ const orm = require("../config/orm.js");
 
 const burger = {
     selectAll: function(cb) {
-        orm.selectAll("cats", function(res) {
+        orm.selectAll("burgers", function(res) {
           cb(res);
         });
       },
       // The variables cols and vals are arrays.
       insertOne: function(cols, vals, cb) {
-        orm.insertOne("cats", cols, vals, function(res) {
+        orm.insertOne("burgers", cols, vals, function(res) {
           cb(res);
         });
       },
       updateOne: function(objColVals, condition, cb) {
-        orm.updateOne("cats", objColVals, condition, function(res) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
           cb(res);
         });
       }
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export the database functions for the controller (burgers_controller.js).
 module.exports = burger;
